2337bcd85be134f842caae7740771f1855e0dc8e
Ktcullen <Kalebcullen32@gmail.com>
2023-05-29 18:53:46 -0600

    Quick Changes

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ import {KillKaneAction} from "./actions/Killkane";
  * @type ConfigType
  */
 export const config = {
-    name: "Dishub",
+    name: "DiscordShield",
     footer: [
         {
             name: {zh: "支持", en: "Support"},
@@ -29,7 +29,7 @@ export const config = {
     },
     tutorialUrl: "https://github.com/SonMooSans/discord-bot-dashboard",
     serverUrl: "http://localhost:8080",
-    inviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1004280473956139038&permissions=8&scope=bot",
+    inviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1112899579675951145&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A8080&response_type=code&scope=bot",
     data: {
         dashboard: dashboardData,
     },
